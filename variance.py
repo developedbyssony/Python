b22a9450088b6bb3c0b428a022d80e491c1c7678
DevelopedByssony <n_uck@naver.com>
2022-02-20 23:48:04 +0900

    220220

---
+++ variance.py
@@ -0,0 +1,8 @@
+import statistics as stat
+def print_value(str, name, num):
+print str % (name, num)
+
+height = [150, 163, 145, 140, 157, 151, 140, 149]
+
+str = "The %s of the list is %.2f"
+print_value(str, "variance", stat.variance(height))
